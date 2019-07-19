5c8c47448a08100e99ddaead514ce41b04af946b
simonzg <zhanghan.simon@gmail.com>
2019-07-19 14:11:27 +0800

    call contract in sequence

--- callContract.js
+++ callContract.js
@@ -15,43 +15,47 @@ const abiDefinition = readAbiDefinition();
 contractInstance = new web3.eth.Contract(abiDefinition);
 contractInstance.options.address = contractAddr;
 
-// notice these calls are executed asynchronously
+// notice these calls are executed synchronously
 contractInstance.methods
   .mintToken(address, "99999999999999999999999")
   .send({ from: address, gas: 4700000 })
-  .then(function(data) {
-    console.log("mintToken result:", data);
+  .then(function(receipt) {
+    console.log("mintToken receipt:", receipt);
+
+    contractInstance.methods
+      .getAccountBalanceOf(address)
+      .call({ from: address, gas: 4700000 })
+      .then(function(data) {
+        console.log("balance: ", data);
+
+        contractInstance.methods
+          ._transferFrom(
+            address,
+            "0x6db04d51beb19b644df783b458db7633b9f90d8b",
+            "9999"
+          )
+          .send({ from: address, gas: 4700000 })
+          .then(function(receipt) {
+            console.log("transfer receipt: ", receipt);
+
+            contractInstance.methods
+              .getAccountBalanceOf(address)
+              .call({ from: address, gas: 4700000 })
+              .then(function(data) {
+                console.log("balance: ", data);
+              })
+              .catch(function(err) {
+                console.log("getAccountBalanceOf error:", err);
+              });
+          })
+          .catch(function(err) {
+            console.log("_transferFrom error:", err);
+          });
+      })
+      .catch(function(err) {
+        console.log("getAccountBalanceOf error:", err);
+      });
   })
   .catch(function(err) {
     console.log("mintToken error:", err);
-  });
-
-contractInstance.methods
-  ._transferFrom(address, "0x6db04d51beb19b644df783b458db7633b9f90d8b", "9999")
-  .send({ from: address, gas: 4700000 })
-  .then(function(data) {
-    console.log("_transferFrom result:", data);
-  })
-  .catch(function(err) {
-    console.log("_transferFrom error:", err);
-  });
-
-contractInstance.methods
-  .getAccountBalanceOf(address)
-  .send({ from: address, gas: 4700000 })
-  .then(function(data) {
-    console.log("getAccountBalanceOf result:", data);
-  })
-  .catch(function(err) {
-    console.log("getAccountBalanceOf error:", err);
-  });
-
-contractInstance.methods
-  .getAccountBalanceOf(address)
-  .call({ from: address, gas: 4700000 })
-  .then(function(data) {
-    console.log("getAccountBalanceOf result:", data);
-  })
-  .catch(function(err) {
-    console.log("getAccountBalanceOf error:", err);
   });
